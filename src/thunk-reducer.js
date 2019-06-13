cf86808a605e198c799e1288cf728228190e27e8
Nathan Buchar <hello@nathanbuchar.com>
2019-06-12 18:05:26 -0700

    fix: Remove instances of redundant useCallback
    Fixes #1

--- src/thunk-reducer.js
+++ src/thunk-reducer.js
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 /**
  * @callback Thunk
@@ -25,19 +25,21 @@ import { useCallback, useRef, useState } from 'react';
 function useThunkReducer(reducer, initialArg, init = (a) => a) {
   const [hookState, setHookState] = useState(init(initialArg));
 
+  // State management.
   const state = useRef(hookState);
-  const getState = useCallback(() => state.current);
-  const setState = useCallback((newState) => {
+  const getState = () => state.current;
+  const setState = (newState) => {
     state.current = newState;
     setHookState(newState);
-  });
+  };
 
-  const reduce = useCallback((action) => reducer(getState(), action));
-  const dispatch = useCallback((action) => (
+  // Reducer and augmented dispatcher.
+  const reduce = (action) => reducer(getState(), action);
+  const dispatch = (action) => (
     typeof action === 'function'
       ? action(dispatch, getState)
       : setState(reduce(action))
-  ));
+  );
 
   return [hookState, dispatch];
 }
