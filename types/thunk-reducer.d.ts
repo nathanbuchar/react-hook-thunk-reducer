ad1103752d5c92dad33dc8b82dc2705695f52a1d
Nathan Buchar <hello@nathanbuchar.com>
2019-06-12 18:09:21 -0700

    feat: Add Typescript types
    Fixes #3

---
+++ types/thunk-reducer.d.ts
@@ -0,0 +1,7 @@
+import { Dispatch, Reducer, useRef, useState } from 'react'
+
+export interface Thunk<S, A> {
+  (dispatch: Dispatch<A | Thunk<S, A>>, getState: () => S): void
+}
+
+export function useThunkReducer<S, A>(reducer: Reducer<S, A>, initialArg: S, init?: (s: S) => S): [S, Dispatch<A | Thunk<S, A>>]
